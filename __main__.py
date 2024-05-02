cf35de3a752f9c92d17a63111de0f52c293af069
Jason R. Coombs <jaraco@jaraco.com>
2024-05-01 21:53:47 -0400

    Fix issue with build not running. Remove any generated pyproject.toml after use.

--- __main__.py
+++ __main__.py
@@ -1,18 +1,25 @@
+import contextlib
 import importlib.resources
 import pathlib
 import runpy
 
 
+@contextlib.contextmanager
 def write_pyproject():
     path = pathlib.Path("pyproject.toml")
     if path.exists():
+        yield
         return
     path.write_text(importlib.resources.files().joinpath("pyproject.toml").read_text())
+    try:
+        yield
+    finally:
+        path.unlink()
 
 
 def run():
-    write_pyproject()
-    runpy.run_module("build")
+    with write_pyproject():
+        runpy.run_module("build", run_name="__main__")
 
 
 __name__ == "__main__" and run()
