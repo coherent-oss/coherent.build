a06a0490d6c8b176dbdcba7ba0e48bfd1f97426f
Jason R. Coombs <jaraco@jaraco.com>
2024-05-05 12:02:49 -0400

    Prefer single quotes

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 __requires__ = [
-    "wheel",
-    "pip-run",
-    "setuptools_scm",
-    "build",
-    "git-fame",
-    "jaraco.context",
-    "requests",
+    'wheel',
+    'pip-run',
+    'setuptools_scm',
+    'build',
+    'git-fame',
+    'jaraco.context',
+    'requests',
 ]
 
 import functools
@@ -33,13 +33,13 @@ from pip_run import scripts
 from jaraco.context import suppress
 
 
-mimetypes.add_type("text/plain", "", strict=True)
-mimetypes.add_type("text/markdown", ".md", strict=True)
-mimetypes.add_type("text/x-rst", ".rst", strict=True)
+mimetypes.add_type('text/plain', '', strict=True)
+mimetypes.add_type('text/markdown', '.md', strict=True)
+mimetypes.add_type('text/x-rst', '.rst', strict=True)
 
 
 def name_from_path():
-    return pathlib.Path(".").absolute().name
+    return pathlib.Path('.').absolute().name
 
 
 def version_from_vcs():
@@ -51,19 +51,19 @@ def summary_from_github():
     return (
         json.loads(
             subprocess.check_output(
-                ["gh", "repo", "view", "--json", "description"],
+                ['gh', 'repo', 'view', '--json', 'description'],
                 text=True,
-                encoding="utf-8",
+                encoding='utf-8',
             )
-        )["description"]
+        )['description']
         or None
     )
 
 
 def python_requires_supported():
-    owner = "python"
-    repo = "cpython"
-    url = f"https://api.github.com/repos/{owner}/{repo}/branches"
+    owner = 'python'
+    repo = 'cpython'
+    url = f'https://api.github.com/repos/{owner}/{repo}/branches'
     branches = requests.get(url).json()
     # cheat and grab the first branch, which is the oldest supported Python version
     return f'>= {branches[0]["name"]}'
@@ -74,13 +74,13 @@ class Filter:
         self.name = name
 
     def __call__(self, info):
-        if info.name == ".":
+        if info.name == '.':
             info.name = self.name
             return info
-        ignore_pattern = "|".join(self.ignored)
-        if re.match(ignore_pattern, info.name.removeprefix("./")):
+        ignore_pattern = '|'.join(self.ignored)
+        if re.match(ignore_pattern, info.name.removeprefix('./')):
             return
-        info.name = self.name + "/" + info.name.removeprefix("./")
+        info.name = self.name + '/' + info.name.removeprefix('./')
         return info
 
 
@@ -102,11 +102,11 @@ class SDist(Filter):
     namespace(name='foo/bar/dist')
     """
 
-    ignored = ["dist", r"(.*[/])?__pycache__$", r"(.*[/])?[.]"]
+    ignored = ['dist', r'(.*[/])?__pycache__$', r'(.*[/])?[.]']
 
 
 class Wheel(Filter):
-    ignored = ["docs", "tests", "README.*", "PKG-INFO", "(meta)"]
+    ignored = ['docs', 'tests', 'README.*', 'PKG-INFO', '(meta)']
 
 
 class ZipInfo(types.SimpleNamespace):
@@ -117,14 +117,14 @@ class ZipInfo(types.SimpleNamespace):
 
 def normalize(name):
     # todo: do proper normalization
-    return name.replace(".", "_")
+    return name.replace('.', '_')
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     metadata = Metadata.from_sdist() or Metadata.discover()
-    root = metadata["Name"].replace(".", "/")
-    filename = pathlib.Path(wheel_directory) / f"{metadata.id}-py3-none-any.whl"
-    with WheelFile(filename, "w") as zf:
+    root = metadata['Name'].replace('.', '/')
+    filename = pathlib.Path(wheel_directory) / f'{metadata.id}-py3-none-any.whl'
+    with WheelFile(filename, 'w') as zf:
         for info in wheel_walk(Wheel(root)):
             zf.write(info.path, arcname=info.name)
         for md_name, contents in make_wheel_metadata(metadata):
@@ -136,25 +136,23 @@ def read_deps():
     """
     Read deps from ``__init__.py``.
     """
-    return scripts.DepsReader.search(["__init__.py"])
+    return scripts.DepsReader.search(['__init__.py'])
 
 
 def make_wheel_metadata(metadata):
-    dist_info = f"{metadata.id}.dist-info"
-    yield f"{dist_info}/METADATA", metadata.render()
-    wheel_md = Metadata(
-        {
-            "Wheel-Version": "1.0",
-            "Generator": "coherent.build",
-            "Root-Is-Purelib": "true",
-            "Tag": "py3-none-any",
-        }
-    )
-    yield f"{dist_info}/WHEEL", wheel_md.render()
+    dist_info = f'{metadata.id}.dist-info'
+    yield f'{dist_info}/METADATA', metadata.render()
+    wheel_md = Metadata({
+        'Wheel-Version': '1.0',
+        'Generator': 'coherent.build',
+        'Root-Is-Purelib': 'true',
+        'Tag': 'py3-none-any',
+    })
+    yield f'{dist_info}/WHEEL', wheel_md.render()
 
 
 def wheel_walk(filter_: Wheel):
-    for root, dirs, files in os.walk("."):
+    for root, dirs, files in os.walk('.'):
         zi = ZipInfo(path=root)
         if not filter_(zi):
             dirs[:] = []
@@ -165,7 +163,7 @@ def wheel_walk(filter_: Wheel):
 
 
 def _to_mapping(fame):
-    return (dict(zip(fame["columns"], row)) for row in fame["data"])
+    return (dict(zip(fame['columns'], row)) for row in fame['data'])
 
 
 class Contributor(types.SimpleNamespace):
@@ -177,17 +175,17 @@ class Contributor(types.SimpleNamespace):
 @suppress(Exception)
 def author_from_vcs():
     # run git-fame twice to get both name and email
-    cmd = ["git-fame", "--format", "json"]
-    names_data = json.loads(subprocess.check_output(cmd, text=True, encoding="utf-8"))
+    cmd = ['git-fame', '--format', 'json']
+    names_data = json.loads(subprocess.check_output(cmd, text=True, encoding='utf-8'))
     emails_data = json.loads(
         subprocess.check_output(
-            cmd + ["--show-email"],
+            cmd + ['--show-email'],
             text=True,
-            encoding="utf-8",
+            encoding='utf-8',
         )
     )
-    names_data["columns"][0] = "name"
-    emails_data["columns"][0] = "email"
+    names_data['columns'][0] = 'name'
+    emails_data['columns'][0] = 'email'
     emails_contribs = _to_mapping(emails_data)
     names_contribs = _to_mapping(names_data)
 
@@ -231,9 +229,9 @@ class Metadata(Message):
             self.add_header(*item)
 
     def _description_in_payload(self):
-        if "Description" in self:
-            self.set_payload(self["Description"])
-            del self["Description"]
+        if 'Description' in self:
+            self.set_payload(self['Description'])
+            del self['Description']
 
     @property
     def id(self):
@@ -245,14 +243,14 @@ class Metadata(Message):
 
     @staticmethod
     def _discover_fields():
-        yield "Metadata-Version", "2.3"
-        yield "Name", name_from_path()
-        yield "Version", version_from_vcs()
-        yield "Author-Email", author_from_vcs()
-        yield "Summary", summary_from_github()
-        yield "Requires-Python", python_requires_supported()
+        yield 'Metadata-Version', '2.3'
+        yield 'Name', name_from_path()
+        yield 'Version', version_from_vcs()
+        yield 'Author-Email', author_from_vcs()
+        yield 'Summary', summary_from_github()
+        yield 'Requires-Python', python_requires_supported()
         for dep in read_deps():
-            yield "Requires-Dist", dep
+            yield 'Requires-Dist', dep
         yield from description_from_readme()
 
     @classmethod
@@ -280,16 +278,16 @@ def guess_content_type(path: pathlib.Path):
 
 @suppress(Exception)
 def description_from_readme():
-    (readme,) = pathlib.Path().glob("README*")
+    (readme,) = pathlib.Path().glob('README*')
     ct = guess_content_type(readme)
     assert ct
-    yield "Description-Content-Type", ct
-    yield "Description", readme.read_text(encoding="utf-8")
+    yield 'Description-Content-Type', ct
+    yield 'Description', readme.read_text(encoding='utf-8')
 
 
 def make_sdist_metadata(metadata) -> tarfile.TarInfo:
-    info = tarfile.TarInfo(f"{metadata.id}/PKG-INFO")
-    file = io.BytesIO(metadata.render().encode("utf-8"))
+    info = tarfile.TarInfo(f'{metadata.id}/PKG-INFO')
+    file = io.BytesIO(metadata.render().encode('utf-8'))
     info.size = len(file.getbuffer())
     info.mtime = time.time()
     return info, file
@@ -297,8 +295,8 @@ def make_sdist_metadata(metadata) -> tarfile.TarInfo:
 
 def build_sdist(sdist_directory, config_settings=None):
     metadata = Metadata.discover()
-    filename = pathlib.Path(sdist_directory) / f"{metadata.id}.tar.gz"
-    with tarfile.open(filename, "w:gz") as tf:
+    filename = pathlib.Path(sdist_directory) / f'{metadata.id}.tar.gz'
+    with tarfile.open(filename, 'w:gz') as tf:
         tf.add(pathlib.Path(), filter=SDist(metadata.id))
         tf.addfile(*make_sdist_metadata(metadata))
     return str(filename)
