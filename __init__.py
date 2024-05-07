8096d6a2020199ae379d1ea4027514c489bfddaf
Jason R. Coombs <jaraco@jaraco.com>
2024-05-07 16:54:28 -0400

    Moved discovery functions to their own module.

--- __init__.py
+++ __init__.py
@@ -12,13 +12,10 @@ __requires__ = [
 import functools
 import importlib.metadata
 import io
-import json
-import mimetypes
 import os
 import pathlib
 import posixpath
 import re
-import subprocess
 import tarfile
 import time
 import types
@@ -28,57 +25,9 @@ from email.message import Message
 from typing import Iterable
 
 import packaging
-import requests
-import setuptools_scm
 from wheel.wheelfile import WheelFile
-from pip_run import scripts
-from jaraco.context import suppress
 
-
-mimetypes.add_type('text/plain', '', strict=True)
-mimetypes.add_type('text/markdown', '.md', strict=True)
-mimetypes.add_type('text/x-rst', '.rst', strict=True)
-
-
-def name_from_path():
-    return pathlib.Path('.').absolute().name
-
-
-def version_from_vcs():
-    return setuptools_scm.get_version()
-
-
-@suppress(subprocess.CalledProcessError)
-def summary_from_github():
-    """
-    Load the summary from GitHub.
-
-    >>> summary_from_github()
-    'A zero-config Python project build backend'
-    """
-    return (
-        json.loads(
-            subprocess.check_output(
-                ['gh', 'repo', 'view', '--json', 'description'],
-                text=True,
-                encoding='utf-8',
-            )
-        )['description']
-        or None
-    )
-
-
-def python_requires_supported():
-    """
-    >>> python_requires_supported()
-    '>= 3...'
-    """
-    owner = 'python'
-    repo = 'cpython'
-    url = f'https://api.github.com/repos/{owner}/{repo}/branches'
-    branches = requests.get(url).json()
-    # cheat and grab the first branch, which is the oldest supported Python version
-    return f'>= {branches[0]["name"]}'
+from . import discovery
 
 
 class Filter:
@@ -143,13 +92,6 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     return str(filename)
 
 
-def read_deps():
-    """
-    Read deps from ``__init__.py``.
-    """
-    return scripts.DepsReader.search(['__init__.py'])
-
-
 def make_wheel_metadata(metadata):
     dist_info = f'{metadata.id}.dist-info'
     yield f'{dist_info}/METADATA', metadata.render()
@@ -171,43 +113,6 @@ def wheel_walk(filter_: Wheel):
 
         children = (ZipInfo(path=os.path.join(root, file)) for file in files)
         yield from filter(None, map(filter_, children))
-
-
-def _to_mapping(fame):
-    return (dict(zip(fame['columns'], row)) for row in fame['data'])
-
-
-class Contributor(types.SimpleNamespace):
-    @property
-    def combined_detail(self):
-        return f'"{self.name}" <{self.email}>'
-
-
-@suppress(Exception)
-def author_from_vcs():
-    # run git-fame twice to get both name and email
-    cmd = ['git-fame', '--format', 'json']
-    names_data = json.loads(subprocess.check_output(cmd, text=True, encoding='utf-8'))
-    emails_data = json.loads(
-        subprocess.check_output(
-            cmd + ['--show-email'],
-            text=True,
-            encoding='utf-8',
-        )
-    )
-    names_data['columns'][0] = 'name'
-    emails_data['columns'][0] = 'email'
-    emails_contribs = _to_mapping(emails_data)
-    names_contribs = _to_mapping(names_data)
-
-    contribs = (
-        Contributor(**val)
-        for val in (
-            {**name_contrib, **email_contrib}
-            for name_contrib, email_contrib in zip(names_contribs, emails_contribs)
-        )
-    )
-    return next(contribs).combined_detail
 
 
 @functools.singledispatch
@@ -254,19 +159,22 @@ class Metadata(Message):
 
     @classmethod
     def discover(cls):
+        """
+        >>> md = Metadata.discover()
+        """
         return cls(cls._discover_fields())
 
     @staticmethod
     def _discover_fields():
         yield 'Metadata-Version', '2.3'
-        yield 'Name', name_from_path()
-        yield 'Version', version_from_vcs()
-        yield 'Author-Email', author_from_vcs()
-        yield 'Summary', summary_from_github()
-        yield 'Requires-Python', python_requires_supported()
-        for dep in read_deps():
+        yield 'Name', discovery.name_from_path()
+        yield 'Version', discovery.version_from_vcs()
+        yield 'Author-Email', discovery.author_from_vcs()
+        yield 'Summary', discovery.summary_from_github()
+        yield 'Requires-Python', discovery.python_requires_supported()
+        for dep in discovery.read_deps():
             yield 'Requires-Dist', dep
-        yield from description_from_readme()
+        yield from discovery.description_from_readme()
 
     @classmethod
     def from_sdist(cls):
@@ -276,28 +184,6 @@ class Metadata(Message):
     def render(self):
         self._description_in_payload()
         return str(self)
-
-
-def guess_content_type(path: pathlib.Path):
-    """
-    >>> guess_content_type('foo.md')
-    'text/markdown'
-    >>> guess_content_type('foo.rst')
-    'text/x-rst'
-    >>> guess_content_type('foo')
-    'text/plain'
-    """
-    type, _ = mimetypes.guess_type(str(path))
-    return type
-
-
-@suppress(Exception)
-def description_from_readme():
-    (readme,) = pathlib.Path().glob('README*')
-    ct = guess_content_type(readme)
-    assert ct
-    yield 'Description-Content-Type', ct
-    yield 'Description', readme.read_text(encoding='utf-8')
 
 
 def make_sdist_metadata(metadata) -> tarfile.TarInfo:
---
+++ discovery.py
@@ -0,0 +1,122 @@
+import json
+import pathlib
+import subprocess
+import types
+import mimetypes
+
+import requests
+import setuptools_scm
+from jaraco.context import suppress
+from pip_run import scripts
+
+
+mimetypes.add_type('text/plain', '', strict=True)
+mimetypes.add_type('text/markdown', '.md', strict=True)
+mimetypes.add_type('text/x-rst', '.rst', strict=True)
+
+
+def name_from_path():
+    return pathlib.Path('.').absolute().name
+
+
+def version_from_vcs():
+    return setuptools_scm.get_version()
+
+
+@suppress(subprocess.CalledProcessError)
+def summary_from_github():
+    """
+    Load the summary from GitHub.
+
+    >>> summary_from_github()
+    'A zero-config Python project build backend'
+    """
+    return (
+        json.loads(
+            subprocess.check_output(
+                ['gh', 'repo', 'view', '--json', 'description'],
+                text=True,
+                encoding='utf-8',
+            )
+        )['description']
+        or None
+    )
+
+
+def python_requires_supported():
+    """
+    >>> python_requires_supported()
+    '>= 3...'
+    """
+    owner = 'python'
+    repo = 'cpython'
+    url = f'https://api.github.com/repos/{owner}/{repo}/branches'
+    branches = requests.get(url).json()
+    # cheat and grab the first branch, which is the oldest supported Python version
+    return f'>= {branches[0]["name"]}'
+
+
+def read_deps():
+    """
+    Read deps from ``__init__.py``.
+    """
+    return scripts.DepsReader.search(['__init__.py'])
+
+
+def _to_mapping(fame):
+    return (dict(zip(fame['columns'], row)) for row in fame['data'])
+
+
+class Contributor(types.SimpleNamespace):
+    @property
+    def combined_detail(self):
+        return f'"{self.name}" <{self.email}>'
+
+
+@suppress(Exception)
+def author_from_vcs():
+    # run git-fame twice to get both name and email
+    cmd = ['git-fame', '--format', 'json']
+    names_data = json.loads(subprocess.check_output(cmd, text=True, encoding='utf-8'))
+    emails_data = json.loads(
+        subprocess.check_output(
+            cmd + ['--show-email'],
+            text=True,
+            encoding='utf-8',
+        )
+    )
+    names_data['columns'][0] = 'name'
+    emails_data['columns'][0] = 'email'
+    emails_contribs = _to_mapping(emails_data)
+    names_contribs = _to_mapping(names_data)
+
+    contribs = (
+        Contributor(**val)
+        for val in (
+            {**name_contrib, **email_contrib}
+            for name_contrib, email_contrib in zip(names_contribs, emails_contribs)
+        )
+    )
+    return next(contribs).combined_detail
+
+
+def guess_content_type(path: pathlib.Path):
+    """
+    >>> guess_content_type('foo.md')
+    'text/markdown'
+    >>> guess_content_type('foo.rst')
+    'text/x-rst'
+    >>> guess_content_type('foo')
+    'text/plain'
+    """
+    type, _ = mimetypes.guess_type(str(path))
+    return type
+
+
+@suppress(Exception)
+def description_from_readme():
+    (readme,) = pathlib.Path().glob('README*')
+    ct = guess_content_type(readme)
+    assert ct
+    yield 'Description-Content-Type', ct
+    yield 'Description', readme.read_text(encoding='utf-8')
