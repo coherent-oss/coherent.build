8153f0b839d90a2f248345294c0a2e837cd1b37a
Jason R. Coombs <jaraco@jaraco.com>
2024-07-28 11:23:00 -0400

    Add some doctests and allow a raw string to be passed.

--- imports.py
+++ imports.py
@@ -1,24 +1,37 @@
 from __future__ import annotations
 
 import ast
+import functools
 import pathlib
 import sys
 
 
-def get_module_imports(module_path: pathlib.Path) -> set[str]:
+@functools.singledispatch
+def get_module_imports(module: pathlib.Path | str) -> set[str]:
     """
     Parse a Python module to extract imported names.
 
+    >>> 'ast' in get_module_imports(pathlib.Path(__file__))
+    True
+
     Excludes relative imports.
+
+    >>> get_module_imports('from . import foo')
+    set()
     """
     return set(
         '.'.join(filter(bool, [getattr(node, 'module', None), alias.name]))
-        for node in ast.walk(ast.parse(module_path.read_text()))
+        for node in ast.walk(ast.parse(module))
         if isinstance(node, ast.Import)
         or isinstance(node, ast.ImportFrom)
         and not node.level
         for alias in node.names
     )
+
+
+@get_module_imports.register
+def _(module: pathlib.Path):
+    return get_module_imports(module.read_text())
 
 
 __name__ == '__main__' and print(list(get_module_imports(pathlib.Path(sys.argv[1]))))
