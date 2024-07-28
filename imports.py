43c61a7aa7a0b8393b39769a33d6ef64c159434e
Jason R. Coombs <jaraco@jaraco.com>
2024-07-28 12:25:21 -0400

    Added check for whether an import is built-in or third-party.

--- imports.py
+++ imports.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 import ast
 import functools
 import pathlib
+import subprocess
 import sys
 
 from typing import Generator
+
+import jaraco.context
 
 
 def rel_prefix(node):
@@ -40,6 +43,29 @@ class Import(str):
         blanks = l_names.count('')
         parents = p_names[:-blanks] if blanks else p_names
         return '.'.join(parents + l_names[blanks:])
+
+    CPE = jaraco.context.ExceptionTrap(subprocess.CalledProcessError)
+
+    @CPE.passes
+    def builtin(self):
+        """
+        Is this import built-in (part of the stdandard library)?
+
+        An import is built-in if it's top-level name is importable
+        without any third-party packages.
+
+        >>> Import('requests').builtin()
+        False
+        >>> Import('urllib.parse').builtin()
+        True
+        >>> Import('os').builtin()
+        True
+        >>> Import('pip').builtin()
+        False
+        """
+        top_level_name = self.split('.')[0]
+        cmd = [sys.executable, '-S', '-c', f'import {top_level_name}']
+        subprocess.check_call(cmd, env={}, stderr=subprocess.DEVNULL)
 
 
 @functools.singledispatch
