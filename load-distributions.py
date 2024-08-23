82376dc4187540d7e154a1be8427ee6bf040d18f
Jason R. Coombs <jaraco@jaraco.com>
2024-08-23 05:07:50 -0400

    Add support for skip

--- load-distributions.py
+++ load-distributions.py
@@ -1,3 +1,4 @@
+import itertools
 import logging
 import re
 
@@ -11,9 +12,15 @@ log = logging.getLogger(__name__)
 
 
 @autocommand.autocommand(__name__)
-def run(include: re.compile = re.compile('.*'), url=pypi.top_8k, jump: str = ''):
+def run(
+    include: re.compile = re.compile('.*'),
+    url=pypi.top_8k,
+    jump: str = '',
+    skip: int = 0,
+):
     logging.basicConfig()
     dists = filter(include.match, pypi.Distribution.query(url=url))
+    dists = itertools.islice(dists, skip, None)
     if jump:
         skipped, dists = split_before(dists, lambda dist: dist == jump, maxsplit=1)
         print('skipped', len(skipped))
