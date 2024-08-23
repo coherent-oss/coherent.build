fff60ed98bf0d91ae6c5d0564c3f3d4d104c9538
Jason R. Coombs <jaraco@jaraco.com>
2024-08-23 12:48:30 -0400

    Emit the dist when an exception is unhandled.

--- load-distributions.py
+++ load-distributions.py
@@ -24,5 +24,9 @@ def run(
     if jump:
         skipped, dists = split_before(dists, lambda dist: dist == jump, maxsplit=1)
         print('skipped', len(skipped))
-    for dist in tqdm.tqdm(list(dists)):
-        dist.load() or dist.save()
+    try:
+        for dist in tqdm.tqdm(list(dists)):
+            dist.load() or dist.save()
+    except Exception:
+        print(f"Unhandled exception processing {dist}")
+        raise
