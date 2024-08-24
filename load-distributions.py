5e66319231e94867e649f217a4ca2596bba49c8a
Jason R. Coombs <jaraco@jaraco.com>
2024-08-24 10:01:02 -0400

    In load-distributions, simply load them into the database. Leave the processing for later.

--- load-distributions.py
+++ load-distributions.py
@@ -1,32 +1,16 @@
-import itertools
-import logging
-import re
+"""
+Load distributions into the database.
+"""
 
 import autocommand
 import tqdm
-from more_itertools import split_before
 
 from . import pypi
-
-log = logging.getLogger(__name__)
 
 
 @autocommand.autocommand(__name__)
 def run(
-    include: re.compile = re.compile('.*'),
     url=pypi.top_8k,
-    jump: str = '',
-    skip: int = 0,
 ):
-    logging.basicConfig()
-    dists = filter(include.match, pypi.Distribution.query(url=url))
-    dists = itertools.islice(dists, skip, None)
-    if jump:
-        skipped, dists = split_before(dists, lambda dist: dist == jump, maxsplit=1)
-        print('skipped', len(skipped))
-    try:
-        for dist in tqdm.tqdm(list(dists)):
-            dist.load() or dist.save()
-    except Exception:
-        print(f"Unhandled exception processing {dist}")
-        raise
+    for dist in tqdm.tqdm(list(pypi.Distribution.query(url=url))):
+        pypi.store().update_one({"id": dist}, {"$set": dist.__json__()}, upsert=True)
