a3a390a1555be840758f5177e93ef3813d1c40c4
Jason R. Coombs <jaraco@jaraco.com>
2024-08-31 03:49:39 -0400

    Sort imports

--- discovery.py
+++ discovery.py
@@ -25,8 +25,7 @@ from more_itertools import unique_everseen
 from packaging.version import Version
 from pip_run import scripts
 
-from ..deps import imports
-from ..deps import pypi
+from ..deps import imports, pypi
 
 log = logging.getLogger(__name__)
 
