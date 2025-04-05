381965bf9fe185fa1ecaeb9c0acde82e7091770d
Jason R. Coombs <jaraco@jaraco.com>
2025-04-05 13:00:12 -0400

    Re-wrote flit module with a render method.

--- flit.py
+++ flit.py
@@ -1,19 +1,13 @@
-import sys
-
 import tomlkit
 
 from . import metadata
 
 
-def write():
-    md = metadata.Message.discover()
+def render(metadata: metadata.Message):
     system = {
         'build-system': {
             'requires': ['flit-core >=3.11, <4'],
             'build-backend': 'flit_core.buildapi',
         }
     }
-    tomlkit.dump(system | md.render_toml(), sys.stdout)
-
-
-__name__ == '__main__' and write()
+    return tomlkit.dumps(system | metadata.render_toml())
