6876558e35b96b119d0701ffae357200293f5b85
Jason R. Coombs <jaraco@jaraco.com>
2025-04-05 10:55:15 -0400

    Render the build system at the top.

--- flit.py
+++ flit.py
@@ -7,14 +7,13 @@ from . import metadata
 
 def write():
     md = metadata.Message.discover()
-    doc = md.render_toml()
-    doc.update({
+    system = {
         'build-system': {
             'requires': ['flit-core >=3.11, <4'],
             'build-backend': 'flit_core.buildapi',
         }
-    })
-    tomlkit.dump(doc, sys.stdout)
+    }
+    tomlkit.dump(system | md.render_toml(), sys.stdout)
 
 
 __name__ == '__main__' and write()
